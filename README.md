ff57009ebfa744aa71593addb388bf4f62ebd4ca
saidone <saidone@saidone.org>
2020-05-11 09:17:55 +0200

    typo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # clj-ssh-keygen
 
-Genereate RSA PKCS #1 key-pairs **from scratch** in Clojure
+Generate RSA PKCS #1 key-pairs **from scratch** in Clojure
 
 ## Usage
 ```clojure
--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-ssh-keygen "0.1.0-SNAPSHOT"
-  :description "Genereate RSA PKCS #1 key-pairs from scratch in Clojure"
+  :description "Generate RSA PKCS #1 key-pairs from scratch in Clojure"
   :url "http://saidone.org"
   :license {:name "MIT"
             :url "https://github.com/saidone75/clj-ssh-keygen/blob/master/LICENSE"}
