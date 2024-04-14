706e09d028d7fd235546ae6cb11b1366a66543c4
saidone <saidone@saidone.org>
2024-04-14 09:07:23 +0200

    better cljdoc-preview.sh

--- cljdoc-preview.sh
+++ cljdoc-preview.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
+# get project name
+PROJECT=$(cat project.clj | grep defproject | cut -d " " -f 2)
+# get version
+VERSION=$(cat project.clj | grep defproject | cut -d " " -f 3 | tr -d '"')
+
+echo "Generating cljdoc for $PROJECT-$VERSION"
+
+# clean up previous run db
 sudo rm -rf ./tmp/cljdoc
 mkdir -p ./tmp/cljdoc
 
+# build and install into local repo
+echo "Installing $PROJECT-$VERSION jar and pom into local repo"
 lein install
 
+# ingest into cljdoc
 docker run --rm \
-  --volume $(pwd):/clj-ssh-keygen \
-  --volume "$HOME/.m2:/root/.m2" \
-  --volume ./tmp/cljdoc:/app/data \
+  -v $(pwd):/$PROJECT \
+  -v "$HOME/.m2:/root/.m2" \
+  -v ./tmp/cljdoc:/app/data \
   --entrypoint clojure \
   cljdoc/cljdoc -Sforce -M:cli ingest \
-    --project clj-ssh-keygen/clj-ssh-keygen \
-    --version 0.2.4-SNAPSHOT \
+    --project clj-ssh-keygen/$PROJECT \
+    --version $VERSION \
     --git /clj-ssh-keygen \
 
-docker run --rm -p 8000:8000 -v ./tmp/cljdoc:/app/data --volume "$HOME/.m2:/root/.m2" cljdoc/cljdoc
+# start server
+docker run --rm -p 8000:8000 -v ./tmp/cljdoc:/app/data -v "$HOME/.m2:/root/.m2" cljdoc/cljdoc
