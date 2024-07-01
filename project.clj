7dbdfb073d933148289105ca9ce094fddb5def67
saidone <saidone@saidone.org>
2024-07-01 08:01:29 +0200

    update Clojure to 1.11.3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/saidone75/clj-ssh-keygen"
   :license {:name "MIT"
             :url "https://github.com/saidone75/clj-ssh-keygen/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.11.2"]]
+  :dependencies [[org.clojure/clojure "1.11.3"]]
   :main ^:skip-aot clj-ssh-keygen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
